f2f0639570622d3a593e978bcacd80ca58f54362
RiccardoVib <riccardo.simionato.vib@gmail.com>
2024-03-30 16:05:13 +0100

    Update Layers.py

--- Code/Layers.py
+++ Code/Layers.py
@@ -6,11 +6,12 @@ parallel_scan = tfp.math.scan_associative
 from einops import repeat
 
 class GLU(tf.keras.layers.Layer):
-    def __init__(self, bias=True, dim=-1, **kwargs):
+    def __init__(self, in_size, bias=True, dim=-1, **kwargs):
         super(GLU, self).__init__(**kwargs)
         self.bias = bias
         self.dim = dim
-        self.dense = tf.keras.layers.Dense(2, use_bias=bias)
+        self.in_size = in_size
+        self.dense = tf.keras.layers.Dense(self.in_size * 2, use_bias=bias)
 
     def call(self, x):
         x = self.dense(x)
