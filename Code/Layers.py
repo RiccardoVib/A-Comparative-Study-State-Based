6ba4da564aacbb86d28771a7f5408880cec2aeeb
RiccardoVib <riccardo.simionato.vib@gmail.com>
2024-07-10 20:25:50 +0200

    Update Layers.py

--- Code/Layers.py
+++ Code/Layers.py
@@ -96,17 +96,20 @@ class S4DKernel(tf.keras.layers.Layer):
         self.N = N
         # Generate dt
         self.H = d_model
+        #self.log_dt = tf.random.uniform([self.H]) * (math.log(dt_max) - math.log(dt_min)) + math.log(dt_min)
         self.log_dt = tf.random.uniform((self.H,), minval=tf.math.log(dt_min), maxval=tf.math.log(dt_max))
 
-        #self.B = tf.Variable(0.5 * tf.ones((self.H, self.N//2)), trainable=True, dtype='float32')
+        B = tf.Variable(0.5 * tf.ones((1, self.H, self.N)), trainable=True, dtype='float32')
+        B = tf.tile(B, [1, 1, 1])
+        self.B = tf.reshape(B, [-1, 1, B.shape[-1]])
 
-        C = tf.complex(tf.random.normal([self.H, self.N // 2]), tf.random.normal([self.H, self.N // 2]))
+        C = tf.complex(tf.random.normal([self.H, self.N]), tf.random.normal([self.H, self.N]))
         self.C = tf.Variable(tf.cast(C, dtype=tf.float32), trainable=True)
 
         self.log_dt = tf.Variable(self.log_dt, trainable=True)
 
-        log_A_real = tf.math.log(tf.constant(0.5 * tf.ones((self.H, self.N//2))))
-        A_imag = tf.constant(np.pi) * repeat(np.arange(N//2), 'n -> h n', h=self.H)
+        log_A_real = tf.math.log(tf.constant(0.5 * tf.ones((self.H, self.N))))
+        A_imag = tf.constant(np.pi) * repeat(np.arange(N), 'n -> h n', h=self.H)
 
         self.log_A_real = tf.Variable(log_A_real, trainable=True)
         self.A_imag = tf.Variable(A_imag, trainable=True)
@@ -131,29 +134,39 @@ class S4DKernel(tf.keras.layers.Layer):
 
         ####States
         # Augment B with state
-        # s = tf.math.real(self.state) / dt
-        # s = s * dtA * tf.exp(dtA) / (tf.exp(dtA) - 1.)
-        # B = tf.concat([s, self.B], axis=-3)  # (1+B H N)
+        B = tf.cast(self.B, dtype=tf.complex64)
+        #dt = tf.expand_dims(dt, axis=1)
+        s = self.state / tf.cast(dt, dtype=tf.complex64)
+        s = s * dtA * tf.exp(dtA) / (tf.exp(dtA) - 1.)
+        B = tf.concat([s, B], axis=0)  # (1+B H N)
         # # Combine B and C
-        # C = tf.reshape(B[:, None, :, :] * C, [-1, self.H, self.N])
+        C = B[:, None, :, :] * C
+        C = tf.reshape(C, [-1, self.H, self.N])
 
-        K = tf.expand_dims(dtA, axis=-1) * tf.cast(tf.range(L), dtype=tf.complex64)  # (H N L)
+
+
         C = C * (tf.exp(dtA) - 1.) / A
-        K = tf.math.real(2 * tf.einsum('hn, hnl -> hl', C, tf.exp(K)))
+        ##K = log_vandermonde(C, dtA, L)  # (H L)
+        K = tf.exp(tf.expand_dims(dtA, axis=-1) * tf.cast(tf.range(L), dtype=tf.complex64))# (H N L)
+        K = tf.tensordot(C, K, axes=[[-1], [-2]])
+        #K = tf.math.real(2 * tf.einsum('hn, hnl -> hl', C, K))
         #K = 2 * tf.reduce_sum(C * tf.exp(K), axis=1, keepdims=False)
 
-        ####States
-        # K = tf.reshape(K, [-1, 1, self.H, L])  # (1+B C H L)
-        # self.state = K[:-1, :, :, :]  # (B C H L)
-        # K = K[-1, :, :, :]  # (C H L)
 
-        return K
+        ####States
+        K = tf.reshape(K, [-1, 1, self.H, L])  # (1+B C H L)
+
+        state = tf.cast(K[:-1, 0, :, :], dtype=tf.complex64)
+        self.state.assign(state)
+        K = K[-1, 0, :, :]  # (C H L)
+
+        return tf.math.real(K)
 
     def reset_states(self):
-        self.state = tf.zeros((self.H, self.N//2), dtype=tf.complex64)
+        self.state = tf.Variable(tf.zeros((self.b_size, self.H, self.N), dtype=tf.complex64), trainable=False)
 
 class S4D(tf.keras.layers.Layer):
-    def __init__(self, d_state=64, d_model=1, **kernel_args):
+    def __init__(self, d_state=64, d_model=1, transposed=True, **kernel_args):
         super().__init__()
 
         self.h = d_model
@@ -168,6 +181,8 @@ class S4D(tf.keras.layers.Layer):
 
         # Pointwise
         self.activation = tf.keras.activations.gelu
+
+        #self.conv = tf.keras.layers.Conv1D(2 * self.h, kernel_size=1, dtype='float32')
         self.glu = GLU(in_size=2*self.n, dim=-1)
 
     def call(self, u):  # absorbs return_output and transformer src mask
@@ -183,6 +198,9 @@ class S4D(tf.keras.layers.Layer):
 
         # Compute D term in state space equation - essentially a skip connection
         y = y + u * tf.expand_dims(self.D, axis=-1)
-        y = self.glu(y)
 
+        #y = self.activation(y)
+        #y = tf.expand_dims(y, axis=-1)
+        #y = self.conv(y)
+        y = self.glu(y)
         return y
